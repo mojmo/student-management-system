168f7d5bf2926a263b4cabbb2920e39f0c616ff6
Mugtaba Mohamed <mojtaba.mohamed187@gmail.com>
2025-06-12 16:38:45 +0300

    fix: modify the `add()` method to accept a Map
    - let `add()` method accept Map data type
    - the expected map keys are `model` for model name, 'obj' for new object, and `fileHeader` for the file header

--- src/models/Storage.java
+++ src/models/Storage.java
@@ -1,7 +1,9 @@
 package models;
 
+import java.util.Map;
+
 public interface Storage<T> {
-    void add(String model, T obj);
+    void add(Map<String, Object> map);
     T get(String model, String id);
     void remove(String model, String id);
     void update(String model, String id, T obj);
