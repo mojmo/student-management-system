2ec3c6e0950462f6d1b1b253f478385b79961823
Mugtaba Mohamed <mojtaba.mohamed187@gmail.com>
2025-06-12 19:11:37 +0300

    fix: modify return type

--- src/models/Storage.java
+++ src/models/Storage.java
@@ -4,7 +4,7 @@ import java.util.Map;
 
 public interface Storage<T> {
     void add(Map<String, Object> map);
-    T get(String model, String id);
+    String get(String model, String id);
     void remove(String model, String id);
     void update(String model, String id, T obj);
 }
